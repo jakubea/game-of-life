efd21441b8dc8d8e5e511b9598a9d77d1ebb6f75
Bea Jakubcova <jakubcova@cngroup.cz>
2019-03-20 16:11:25 +0100

    Set initial canvas rendering

--- src/Main.elm
+++ src/Main.elm
@@ -2,104 +2,102 @@ module Main exposing (..)
 
 import Browser
 import Browser.Events exposing (onAnimationFrame)
-import Canvas exposing (..)
+import Canvas
 import Color exposing (Color)
 import Grid
 import Html exposing (Html)
-import Html.Events exposing (onClick)
+import Html.Attributes
+import Html.Events
 import Time exposing (Posix)
 
 
 type alias Model =
-    ( Bool, Float )
+    { aliveCellList : List Cell
+    , isRunning : Bool
+    , time : Float
+    }
+
+
+type alias Cell =
+    { x : Int
+    , y : Int
+    }
 
 
 type Msg
     = AnimationFrame Posix
-    | ToggleRunning
-
-
-main : Program () Model Msg
-main =
-    Browser.element { init = init, update = update, subscriptions = subscriptions, view = view }
-
-
-subscriptions : Model -> Sub Msg
-subscriptions ( isRunning, time ) =
-    if isRunning then
-        onAnimationFrame AnimationFrame
-
-    else
-        Sub.none
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( ( True, 0 )
+    ( { aliveCellList =
+            [ Cell 0 0
+            , Cell 1 1
+            , Cell 2 2
+            , Cell 3 2
+            ]
+      , isRunning = True
+      , time = 0
+      }
     , Cmd.none
     )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg ( isRunning, time ) =
+update msg model =
     case msg of
         AnimationFrame t ->
-            ( ( isRunning, t |> Time.posixToMillis |> toFloat ), Cmd.none )
-
-        ToggleRunning ->
-            ( ( not isRunning, time ), Cmd.none )
+            ( { model | time = t |> Time.posixToMillis |> toFloat }, Cmd.none )
 
 
-h : Float
-h =
+height : Float
+height =
     500
 
 
-w : Float
-w =
+width : Float
+width =
     500
 
 
+gridSize : Int
 gridSize =
     24
 
 
-gridSizef =
-    toFloat gridSize
-
-
+cellSize : Float
 cellSize =
-    (w - padding * 2) / gridSizef
+    (width - padding * 2) / toFloat gridSize
 
 
+padding : Float
 padding =
-    w * 0.1
+    width * 0.1
 
 
-length =
-    cellSize * 0.65
-
-
+thickness : Float
 thickness =
-    cellSize * 0.1
+    cellSize * 0.5
 
 
 view : Model -> Html Msg
-view ( isRunning, time ) =
-    toHtml ( round w, round h )
-        [ onClick ToggleRunning ]
-        (shapes [ fill Color.white ] [ rect ( 0, 0 ) w h ]
-            :: renderItems time
+view model =
+    Canvas.toHtml ( round width, round height )
+        []
+        (Canvas.shapes [ Canvas.fill Color.white ]
+            [ Canvas.rect ( 0, 0 ) width height
+            ]
+            :: renderItems model
         )
 
 
-renderItems time =
-    Grid.fold2d { rows = gridSize, cols = gridSize }
-        (renderItem time)
-        []
+renderItems : Model -> List Canvas.Renderable
+renderItems model =
+    List.map (\{ x, y } -> renderItem ( x, y )) model.aliveCellList
 
 
-renderItem time ( col, row ) lines =
+renderItem : ( Int, Int ) -> Canvas.Renderable
+renderItem ( col, row ) =
     let
         ( colf, rowf ) =
             ( toFloat col, toFloat row )
@@ -108,35 +106,27 @@ renderItem time ( col, row ) lines =
             ( (rowf * cellSize) + padding + cellSize / 2
             , (colf * cellSize) + padding + cellSize / 2
             )
-
-        ( u, v ) =
-            ( colf / (gridSizef - 1)
-            , rowf / (gridSizef - 1)
-            )
-
-        offset =
-            u * 0.4 + v * 0.2
-
-        t =
-            time / 1000
-
-        rotationModifier =
-            sin (t + offset) ^ 3
-
-        initialRotation =
-            degrees 90
-
-        rotation =
-            initialRotation + rotationModifier * pi
     in
-    shapes
-        [ transform [ translate x y, rotate rotation, translate -x -y ]
-        , fill Color.black
+    Canvas.shapes
+        [ Canvas.fill Color.black
         ]
-        [ rect ( x - length / 2, y - thickness / 2 ) length thickness ]
-        :: lines
+        [ Canvas.circle ( x - thickness, y - thickness ) thickness ]
 
 
-lerp : Float -> Float -> Float -> Float
-lerp v0 v1 t =
-    v0 * (1 - t) + v1 * t
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions { isRunning, time } =
+    if isRunning then
+        onAnimationFrame AnimationFrame
+
+    else
+        Sub.none
